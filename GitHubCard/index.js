0319b49830477adffffd812a718fbaf09bbc09c5
Garin Mccullick <glmccullick@gmail.com>
2021-04-09 01:12:47 -0500

    Promises

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -27,13 +27,99 @@
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
-
+import axios from "axios";
+ 
+const result = axios.get("https://api.github.com/users/GarinMcCullick")
+result.then(futureData =>{
+  console.log(futureData)
+  console.log(futureData.data)
+})
+.catch(err => {
+  console.log(err)
+})
 const followersArray = [];
+const entryPoint = document.body.querySelector(".card")
+
+function cardMaker(obj){
+  // create all my elements
+  const divCard = document.createElement("div");
+  const imgURL = document.createElement("img");
+  const divInfo = document.createElement("div");
+  const h3Name = document.createElement("h3");
+  const pUserName = document.createElement("p");
+  const pLocation = document.createElement("P");
+  const pProfile = document.createElement("p");
+  const link = document.createElement("a");
+  const pFollowers = document.createElement("p");
+  const pFollowing = document.createElement("p");
+  const pBio = document.createElement("p");
+
+
+  h3Name.textContent = obj.name;//finally
+  pUserName.textContent = obj.username;
+  pLocation.textContent = obj.location;
+  pFollowers.textContent =obj.followers;
+  pFollowing.textContent =obj.following;
+  pBio.textContent = obj.bio;
+  link.textContent = obj.html_url;
+  pProfile.textContent = obj.html_url;
+
+  //setting class names/ referencing urls.
+  divCard.classList.add("card");
+  divInfo.classList.add("card-info");
+  h3Name.classList.add("name");
+  imgURL.src = obj.avatar_url;
+  link.href = obj.html_url;
+
+  //heirarchy
+  divCard.appendChild(imgURL);
+  divCard.appendChild(divInfo);
+
+  divInfo.appendChild(h3Name);
+  divInfo.appendChild(pUserName);
+  divInfo.appendChild(pLocation);
+  divInfo.appendChild(pProfile);
+
+  pProfile.appendChild(link);
+
+  divInfo.appendChild(pFollowers);
+  divInfo.appendChild(pFollowing);
+  divInfo.appendChild(pBio);
+
+  
+  return divCard//returning this because its my largest div...
+}
+console.log(cardMaker(name));
+
+axios
+.get("https://api.github.com/users/GarinMcCullick")
+.then(response=>{
+  console.log("RESPONSE", response.data)
+ const resp = response.data;
+ resp.forEach(obj => {
+    const card = cardMaker({obj})
+    console.log(card)
+    entryPoint.append(card)
+  })
+})
+.catch((err) => {
+  debugger;
+});
+
+fetch("https://api.github.com/users/GarinMcCullick")
+  .then(res =>{
+    return res.json()
+  })
+  .then(parsedJSON => {
+    debugger;
+  })
+  .catch(err =>{
+    debugger;
+  })
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
